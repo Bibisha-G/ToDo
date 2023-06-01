93c9d015af821486f859e286c5744261b31e77d3
Bibisha-G <bibishaguragai656@gmail.com>
2023-06-01 23:11:31 +0545

    updated js, css

--- todolist.js
+++ todolist.js
@@ -1,52 +1,29 @@
-document.addEventListener('DOMContentLoaded',function() {
 
-    //by default the submit button should be disabled
-    document.querySelector('#submit').disabled = true;
-
-    document.querySelector('#addtask').onkeyup=() =>{
-        if(document.querySelector('#addtask').value.length>0){
-            document.querySelector('#submit').disabled=false;
-            
-        }else{
-            document.querySelector('#submit').disabled = true;
-        }  
-    }
-
-
-    document.querySelector('form').onsubmit=function() {
-        const taskForm = document.querySelector('#task-form');
-
-       const task= document.querySelector('#addtask').value ;
-       const li = document.createElement('li');
-       li.innerHTML=task;
-
-       document.querySelector('#tasks').append(li);
-       document.querySelector('#addtask').value='';
-       document.querySelector('#submit').disabled = true; // submit button becomes in active again after submtting a task
-    
-       // Add event listener to the form for submit event
-    taskForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent form submission
-
-        addTask();
+  document.addEventListener('DOMContentLoaded', function() {
+    const form = document.querySelector('form');
+    const taskInput = document.querySelector('#addtask');
+    const taskList = document.querySelector('#tasks');
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
+  
+      const task = taskInput.value;
+  
+      if (task.trim() !== '') { // ensures that the task variable contains a non-empty value after removing any leading or trailing whitespace.
+        const listItem = document.createElement('li');
+        const taskLabel = document.createElement('span');
+        taskLabel.textContent = task;
+  
+        taskLabel.addEventListener('click', function() {
+          taskLabel.style.textDecoration = 'line-through';
+        });
+  
+        listItem.append(taskLabel);
+        taskList.append(listItem); //adds the new task at the end of task list 
+  
+        taskInput.value = ''; // Clear the input field
+        taskInput.focus(); // Set focus back to the input field
+      }
     });
-
-
-
-        // Save the task to local storage
-        const tasksFromStorage = localStorage.getItem('tasks');
-        let tasks = [];
-
-        if (tasksFromStorage) {
-            tasks = JSON.parse(tasksFromStorage);
-        }
-
-        tasks.push(task);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-
-        //since it is  form an on submttting it takes to another page, we need to stop it from submittng
-         return false; 
-
-    };
-
-});
+  });
+   
